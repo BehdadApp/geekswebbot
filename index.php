59c78fa1b4f7120e97eee0ff0613c97e05ffaf0c
Mohammad Behdad <m.behdad7@gmail.com>
2016-10-29 22:24:22 +0330

    Update

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ try {
 		$mappost="idUser=5" . "&Type=" . "2" . "&Cat=10" . "&Title=" ."333". "&Des=" ."33333333". "&TelegramLink=" .$Tlink. "&Special=0" . "&Image=f3edc3964a03a5bc0086c1238afa9dc6.jpg" ;
 		
 		//header('Location: http://srv.parperook.ir/TeleBazaar/AddFromBot.php?'.$mappost);
-			
+		$lines = file("http://srv.parperook.ir/TeleBazaar/AddFromBot.php?'.$mappost");	
 		
 		$response = $client->sendChatAction(['chat_id' => '-121921633', 'action' => 'typing']);
    	 	$response = $client->sendMessage([
